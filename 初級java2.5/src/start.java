240d48284d095a0fe9bca29bd4aed4fbe3a9ec0c
sasahara <zeal23310180@fsgcl.jp>
2024-05-13 12:07:59 +0900

    練習問題をコミット（エラーが直らないままです。）

---
+++ 初級java2.5/src/start.java
@@ -0,0 +1,15 @@
+
+public class start {
+
+	public static void main(String[] args) {
+		int x=2;
+		int y=3;
+		System.out.println(x*y);
+		
+        MyFrame frame2=new RectFrame();
+        frame2.setLocation(350,200);
+        MyFrame frame3=new RectFrame();
+        frame3.setLocation(50,300);
+	}
+
+}
